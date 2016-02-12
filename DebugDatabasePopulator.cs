b131f6c9d7b90f038730412c726d1ea20caa4ccc
Pawel Pietrasz <pawesong@gmail.com>
2016-02-12 17:35:52 +0100

    Added automatic database populator to speed up testing

---
+++ DebugDatabasePopulator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using FileRepo.Model;
+
+namespace FileRepo
+{
+    public class DebugDatabasePopulator
+    {
+        private void Populate(RepoContext db)
+        {
+            var term = new Term
+            {
+                TermNumber = 1
+            };
+            db.Terms.Add(term);
+
+            var subject = new Subject
+            {
+                Name = "Programowanie w C",
+                Term = term
+            };
+            db.Subjects.Add(subject);
+
+            var user = new User
+            {
+                Admin = true,
+                Allowed = true,
+                Guid = Guid.NewGuid(),
+                Name = "Paweł Pietrasz",
+                RegistrationDate = DateTime.Now,
+                UserName = "559071237590207",
+                VerificationDate = DateTime.Now,
+            };
+            db.Users.Add(user);
+
+            db.SaveChanges();
+        }
+
+        public void EnsurePopulated()
+        {
+            var db = new RepoContext();
+            if (!db.Terms.Any())
+                Populate(db);
+            db.Dispose();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Linq;
 using Nancy.Hosting.Self;
-using Microsoft.Data.Entity;
-using FileRepo.Model;
 
 namespace FileRepo
 {
@@ -10,34 +7,8 @@ namespace FileRepo
     {
         static void Main(string[] args)
         {
-            /*var db = new RepoContext();
-            var term = new Term
-            {
-                TermNumber = 1
-            };
-            var subject = new Subject
-            {
-                Name = "Programowanie w C",
-                Term = term
-            };
-            var file = new Item
-            {
-                DateAdded = DateTime.Now,
-                Description = "to jest taki dość przydługawy opisik pliczku wrzuconego przez studencika na serwerek",
-                Name = "plik.jpg",
-                Subject = subject,
-                User = db.Users.First()
-            };
-            db.Terms.Add(term);
-            db.Subjects.Add(subject);
-            db.Items.Add(file);
-            db.SaveChanges();*/
-
-            /*var db = new RepoContext();
-            var plik = db.Items.First();
-            plik.Notes = "dupa";
-            db.Update(plik);
-            db.SaveChanges();*/
+            var populator = new DebugDatabasePopulator();
+            populator.EnsurePopulated();
 
             var uri = new Uri("http://localhost:3579");
 
