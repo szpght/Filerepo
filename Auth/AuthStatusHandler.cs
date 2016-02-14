60d4a5c3f65172df00d126f43bfa1a6906297049
Pawel Pietrasz <pawesong@gmail.com>
2016-02-14 19:26:39 +0100

    Created AuthStatusHandler, list of handled codes implemented

---
+++ Auth/AuthStatusHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.ErrorHandling;
+
+namespace FileRepo.Auth
+{
+    public class AuthStatusHandler : IStatusCodeHandler
+    {
+        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        {
+            if (statusCode == HttpStatusCode.Forbidden || statusCode == HttpStatusCode.Unauthorized)
+                return true;
+            return false;
+        }
+    }
+}
